b5ba4740cf0e138ee0f2fd7d8e1bbde9969eba7a
Riguz Lee <solee.linux@gmail.com>
2019-08-22 10:32:21 +0800

    created project by spring initializr

---
+++ src/main/java/com/thoughtworks/computerinventory/ComputerInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.computerinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerInventoryApplication.class, args);
+	}
+
+}
